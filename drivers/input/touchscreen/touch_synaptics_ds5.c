122e9c122ce70df3f980e7653f68958c7d4c50a4
Dennis Rassmann <showp1984@gmail.com>
2013-11-18 22:10:49 +0100

    touch_synaptics_ds5: remove wakeup procedure for i2c device
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- drivers/input/touchscreen/touch_synaptics_ds5.c
+++ drivers/input/touchscreen/touch_synaptics_ds5.c
@@ -1725,7 +1725,7 @@ static int lcd_notifier_callback(struct notifier_block *this,
 		}
 		mutex_unlock(&ts->input_dev->mutex);
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE_PREVENT_SLEEP
-		if (device_may_wakeup(&ts->client->dev))
+		if (s2w_switch > 0)
 			disable_irq_wake(ts->client->irq);
 #endif
 		break;
@@ -1748,7 +1748,7 @@ static int lcd_notifier_callback(struct notifier_block *this,
 			mutex_unlock(&ts->input_dev->mutex);
 		}
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE_PREVENT_SLEEP
-		if (device_may_wakeup(&ts->client->dev))
+		if (s2w_switch > 0)
 			enable_irq_wake(ts->client->irq);
 #endif
 		break;
@@ -1886,7 +1886,6 @@ static int synaptics_ts_probe(
 	ret = request_threaded_irq(client->irq, NULL, touch_irq_handler,
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE_PREVENT_SLEEP
 			IRQF_TRIGGER_FALLING | IRQF_ONESHOT | IRQF_NO_SUSPEND, client->name, ts);
-	device_init_wakeup(&client->dev, 1);
 #else
 			IRQF_TRIGGER_FALLING | IRQF_ONESHOT, client->name, ts);
 #endif
